761c56a87789079e388091a859c4ea84bd2fd82d
rease <cftcview@gmail.com>
2022-08-18 21:58:37 +0500

    fix

--- src/__App.rease.client.ts
+++ src/__App.rease.client.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+// @ts-nocheck
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import {
   _c as _c7,
--- src/__Title.rease.client.ts
+++ src/__Title.rease.client.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+// @ts-nocheck
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import {
   _$ as _$1,
